5b80d1c0c8b875c861b6f73408d57a3021d26b62
Tobias Setterborg <setterborg.tobias@gmail.com>
2017-05-11 19:19:29 +0200

    Working width navigation transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,6 +58,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function openSection(e) {
         let target = e.target.closest('li').dataset.id;
+
+        console.log(document.body.classList);
+
+        if (document.body.classList.contains('twitter-visible')) {
+            document.body.classList = '';
+            return setTimeout(() => document.body.classList = target + '-visible', 500);
+        }
         
         if (document.body.classList == target + '-visible') {
             return document.body.classList = '';  
